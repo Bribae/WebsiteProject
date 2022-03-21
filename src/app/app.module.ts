7e4a47c3dda921aff0ffa3299479071bbfbd7aae
myname <noah@s56.net>
2022-03-21 16:42:24 -0600

    added admin home, admin lognin, message 1, and home component, created a dark theme, and did some small detail work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {Routes, RouterModule} from "@angular/router";
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './Navigation/Navigation.component';
-import { SignInComponent } from './sign-in/sign-in.component';
+import { NavigationComponent } from './Components/Navigation/Navigation.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
@@ -11,14 +11,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
-import { ErrorComponent } from './error/error.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { ErrorComponent } from './Components/error/error.component';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { AdminLoginComponent } from './Components/admin-login/admin-login.component';
+import { Message1Component } from './Components/message1/message1.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AdminHomeComponent } from './Components/admin-home/admin-home.component';
 
 
 const routes: Routes = [
   {path:'signin', component: SignInComponent},
   {path: 'error', component: ErrorComponent},
-  {path: 'signin/signup', component: SignUpComponent}
+  {path: 'signup', component: SignUpComponent},
+  {path: 'adminlogin', component: AdminLoginComponent},
+  {path: 'message/1', component: Message1Component},
+  {path: '', component: HomeComponent},
+  {path: 'adminhome', component: AdminHomeComponent}
 ];
 
 
@@ -29,6 +37,10 @@ const routes: Routes = [
     SignInComponent,
     ErrorComponent,
     SignUpComponent,
+    AdminLoginComponent,
+    Message1Component,
+    HomeComponent,
+    AdminHomeComponent,
   ],
   imports: [
     BrowserModule,
